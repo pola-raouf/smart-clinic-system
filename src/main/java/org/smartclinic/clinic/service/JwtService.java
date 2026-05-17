e9e0102929ec5670cbece760a375305a4e6b888a
Peter Emad <peteromda34@gmail.com>
2026-05-17 21:42:48 +0300

    Update JwtService.java

--- src/main/java/org/smartclinic/clinic/service/JwtService.java
+++ src/main/java/org/smartclinic/clinic/service/JwtService.java
@@ -10,15 +10,15 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    // 🔐 secret key (must be long enough)
+    
     private final String SECRET_KEY = "smartclinicsecretkeysmartclinicsecretkey";
 
-    // 🔑 generate signing key
+    
     private Key getSignKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
 
-    // 🎯 Generate JWT token
+    
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
@@ -28,7 +28,7 @@ public class JwtService {
                 .compact();
     }
 
-    // 📥 Extract email from token
+    
     public String extractEmail(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getSignKey())
@@ -38,7 +38,7 @@ public class JwtService {
                 .getSubject();
     }
 
-    // ✅ Validate token
+    
     public boolean isTokenValid(String token, String email) {
         return extractEmail(token).equals(email);
     }
